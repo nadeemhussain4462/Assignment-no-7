2780ca540edf05cf436d01362061bad830409c63
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-09-20 10:42:09 +0500

    assignement 7

---
+++ app.js
@@ -0,0 +1,22 @@
+// Assignment no 7 
+// Palindrome words 
+
+
+// for (var i = 0; i <= 5; i++) {
+//     var word = prompt("Enter a word to check if it's a palindrome:");
+//     var isPalindrome = true;
+//     for (var j = 0; j < word.length / 2; j++) {
+//         if (word[j] !== word[word.length - 1 - j]) {
+//             isPalindrome = false;
+//             break;
+//         }
+//     }
+//     if (isPalindrome) {
+//         console.log(word + " is a palindrome.");
+//     } else {
+//         console.log(word + " is not a palindrome.");
+//     }
+// }
+
+
+
