195652f215036a670cba8bf0c4b3424d6f3eaa18
Nadeem Hussain <hoteleyenadeem@gmail.com>
2024-09-20 11:13:27 +0500

    assignment 7 done Palindrome word

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // Assignment no 7 
-// Palindrome words 
+// Palindrome words using split, reverse & join metthod
 
+
+var word = prompt("Enter a word to check if it's a palindrome:");
+var reversedWord = word.split("").reverse().join("");
+if (word === reversedWord) {
+    console.log(word + " is a palindrome.");
+} else {
+    console.log(word + " is not a palindrome.");
+}
+
+
+
+// Palindrome words using for loop and nested
 
 // for (var i = 0; i <= 5; i++) {
 //     var word = prompt("Enter a word to check if it's a palindrome:");
@@ -20,3 +32,13 @@
 
 
 
+
+// var arr = "asif ahmed"
+// console.log(arr.split());
+
+// // Reverse Array Method:
+
+
+// var arr = ["asif", "ahmed", "ali","khan","raza"]
+// console.log(arr.reverse());
+// console.log(arr.reverse().join(" "))
